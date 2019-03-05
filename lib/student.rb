2303b43d32b141630122933583bfb66cbe1df5b4
Lizzie Chan <lchan217@gmail.com>
2019-03-05 21:50:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    attributes_hash.each do |student|
+    attributes_hash.each do |symbol|
       binding.pry
     end 
   end
