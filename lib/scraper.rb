2af52b15e6595ee73096297806d6a8d7d27ec4a0
Lizzie Chan <lchan217@gmail.com>
2019-03-05 21:11:14 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -51,7 +51,7 @@ class Scraper
     student_hash[:profile_quote] = doc.css('.profile-quote').text
     
     student_hash[:bio] = doc.css("div.bio-content p").text
-    #binding.pry
+    binding.pry
     student_hash
   end
 
