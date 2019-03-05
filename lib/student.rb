414e565d71ce6461d5796235fd0610d1cf5f0ad8
Lizzie Chan <lchan217@gmail.com>
2019-03-05 21:42:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,6 +9,14 @@ class Student
   def initialize(student_hash)
     
   end
+  
+  
+  class Person
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+  def initialize(attributes)
+     attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
+
 
   def self.create_from_collection(students_array)
     
