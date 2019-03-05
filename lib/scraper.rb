7b55c4220b3c166d9a74284d58105e5a70a175f0
Lizzie Chan <lchan217@gmail.com>
2019-03-05 19:58:47 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,14 +1,43 @@
+require 'nokogiri'
 require 'open-uri'
 require 'pry'
 
 class Scraper
-
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+
+    main = doc.css('.student-card')
+    array_of_hashes = [] 
+    main.map do |person|
+       array_of_hashes << {:name => person.css('.card-text-container').css('.student-name').text, 
+        :location =>  person.css('.card-text-container').css('.student-location').text, 
+        :profile_url => person.css('a[href^="students/"]').first.values.join
+        }
+    end
+    array_of_hashes
   end
 
   def self.scrape_profile_page(profile_url)
-    
+    html = open(profile_url)
+    doc = Nokogiri::HTML(html)
+   
+    main = doc.css('.social-icon-container')
+    student_hash = {}
+    main.map do |x|
+      if main.css('a[href*="twitter"]').first.values.join.include?("twitter")
+        student_hash[:twitter] =  main.css('a[href*="twitter"]').first.values.join 
+      elsif main.css('a[href*="linkedin"]').first.values.join.include?("linkedin")
+        student_hash[:linkedin] = main.css('a[href*="linkedin"]').first.values.join
+      elsif main.css('a[href*="github"]').first.values.join.include?("github")
+        student_hash[:github] = main.css('a[href*="github"]').first.values.join
+      else
+        student_hash[:blog] = main.css('a[href]')
+      end
+    end 
+    student_hash[:profile_quote] = main.css('.profile-quote').text
+    student_hash[:bio] = doc.css("div.bio-content p").text
+    student_hash
   end
 
 end
