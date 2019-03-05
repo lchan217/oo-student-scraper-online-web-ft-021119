70f4c5c6f5367c39e708e0d41e59bc91dc8acfdb
Lizzie Chan <lchan217@gmail.com>
2019-03-05 21:48:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,8 +14,8 @@ class Student
   def self.create_from_collection(students_array)
     students_array.each do |student| 
       self.new(student)
-    end
     binding.pry
+  end
   end
 
   def add_student_attributes(attributes_hash)
