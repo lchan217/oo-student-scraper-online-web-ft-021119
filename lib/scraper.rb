6880032d25bf9b570df1789b34bdb3fd43ab9c3e
Lizzie Chan <lchan217@gmail.com>
2019-03-05 20:51:09 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,7 +27,7 @@ class Scraper
     student_hash = {}
     main.map do |x|
       binding.pry
-      if !!main.css('a[href*="twitter"]').first
+      if main.css('a[href*="twitter"]').first != nil
         student_hash[:twitter] =  main.css('a[href*="twitter"]').first.values.join
       end
       if main.css('a[href*="linkedin"]').first.values.join.include?("linkedin")
