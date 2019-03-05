89e877dca1cad248c62741911a15bf50d4485b1a
Lizzie Chan <lchan217@gmail.com>
2019-03-05 20:58:19 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -36,7 +36,7 @@ class Scraper
       if main.css('a[href*="github"]').first != nil
         student_hash[:github] = main.css('a[href*="github"]').first.values.join
       end
-      if main.css('a[href]')[-1].values != "#" 
+      if main.css('a[href]')[].values != "#" 
         student_hash[:blog] = main.css('a[href]')[-1].values.join
       end
     end 
